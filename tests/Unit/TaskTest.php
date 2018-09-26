a94fa0bd0249e23ff615f28f6f18daf5e96e0e9b
RushOT <rashot1995@gmail.com>
2018-09-26 16:18:50 +0500

    Some plain tests are added

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -8,13 +8,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+
+         public function testEmpty()
+         {
+             $value = [];
+             $this->assertEmpty($value);
+             return $value;
+         }
+         /**
+          * @depends testEmpty
+          */
+         public function testPush(array $value)
+         {
+             array_push($value, 'first');
+             $this->assertEquals('first', $value[count($value) - 1]);
+             $this->assertNotEmpty($value);
+             return $value;
+         }
+
+         public function testFailure(){
+           $this->assertContains(4, [1, 2, 3]);
+         }
 }
